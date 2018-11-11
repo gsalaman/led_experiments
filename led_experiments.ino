a446a911e6de9c1ca25caccc591a1420e2d8fe08
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-11-11 08:21:42 -0700

    1st cut...not compiled.

--- led_experiments.ino
+++ led_experiments.ino
@@ -1,6 +1,5 @@
 /*===========================================================
- * First test sketch.  Use leds array to set circles to blue, then
- * walk a red light around.  
+ * Implementing helper functions.
  */
 
 #include <SoftwareSerial.h>
@@ -13,6 +12,162 @@
 CRGB leds[NUM_LEDS];
 
 #define BRIGHTNESS          40
+
+// a bunch of helpful defines
+#define INNER_START 0
+#define NUM_INNER  16
+#define OUTER_START 16
+#define NUM_OUTER 24
+#define LAST_INNER (NUM_INNER - 1)
+#define LAST_OUTER (NUM_LEDS - 1)
+
+/*===============================================================================
+ * Function:  fill_inner
+ */
+void fill_inner(CRGB color)
+{
+    fill_solid(leds, NUM_INNER, color);
+}
+
+/*===============================================================================
+ * Function:  fill_outer
+ */
+void fill_outer(CRGB color)
+{
+    // Two ways of doing the pointer math.  I'm gonna try both and see which one 
+    // is more efficient....
+    fill_solid(&(leds[OUTER_START]), NUM_OUTER, color);
+    // fill_solid(leds+OUTER_START*sizeof(CRGB), NUM_OUTER, color);
+}
+
+#ifndef ROTATE_DOWN_PTR
+/*===============================================================================
+ * Function:  rotate_down_helper
+ *
+ * Since the LEDs can be wired arbitrarily (is increasing the index going clockwise
+ * or counter-clockwise), I'm gonna make these helpers just go "up" for increasing array
+ * and "down" for decreasing array.  We can then map to clockwise and counter-clockwise
+ * with the real rotate functions.
+ */
+void rotate_down_helper( CRGB *start_led, int NUM_LEDS )
+{
+    CRGB roll_over_value;
+    int i;
+    
+    // this is basically just gonna be a big shift with roll-over.
+    
+    // remember the "0th" value...it's gonna go into the "last" array value.
+    roll_over_value = start_led[0];
+    
+    // now copy everything one slot "down"
+    for (i=0; i< NUM_LEDS -1; i++)
+    {
+        start_led[i] = start_led[i+1];
+    }
+    
+    // Finally, store the last LED with that roll-over value.
+    start_led[NUM_LEDS - 1] = roll_over_value;
+    
+}  // end of rotate_down_helper
+#else
+/*===============================================================================
+ * Function:  rotate_down_helper
+ *
+ * Since the LEDs can be wired arbitrarily (is increasing the index going clockwise
+ * or counter-clockwise), I'm gonna make these helpers just go "up" for increasing array
+ * and "down" for decreasing array.  We can then map to clockwise and counter-clockwise
+ * with the real rotate functions.
+ */
+void rotate_down_helper( CRGB *start_led, int NUM_LEDS )
+{
+    CRGB roll_over_value;
+    int i;
+    
+    // this is basically just gonna be a big shift with roll-over.
+    
+    // remember the "0th" value...it's gonna go into the "last" array value.
+    roll_over_value = *start_led;
+    
+    // now copy everything one slot "down"
+    while (i < NUM_LED - 1)
+    {
+        *start_led = *(start_led+sizeof(CRGB));
+        i++;
+        start_led++;
+    }
+    
+    // Finally, store the last LED with that roll-over value.
+    *start_led = roll_over_value;
+    
+}  // end of rotate_down_helper
+#endif
+
+/*===============================================================================
+ * Function:  rotate_up_helper
+ *
+ * Since the LEDs can be wired arbitrarily (is increasing the index going clockwise
+ * or counter-clockwise), I'm gonna make these helpers just go "up" for increasing array
+ * and "down" for decreasing array.  We can then map to clockwise and counter-clockwise
+ * with the real rotate functions.
+ */
+void rotate_up_helper( CRGB *start_led, int NUM_LEDS )
+{
+    CRGB roll_over_value;
+    int i;
+    
+    // this is basically just gonna be a big shift with roll-over.
+    
+    // remember the "last" value...it's gonna go into the "first" array value.
+    roll_over_value = start_led[NUM_LEDS - 1];
+    
+    // now copy everything one slot "up"
+    for (i=NUM_LEDS - 1; i > 0; i--)
+    {
+        start_led[i] = start_led[i-1];
+    }
+    
+    // Finally, store the first LED with that roll-over value.
+    start_led[0] = roll_over_value;
+    
+}  // end of rotate_down_helper
+
+/*===============================================================================
+ * Function:  rotate_inner_clockwise
+ */
+void rotate_inner_clockwise( void )
+{
+    rotate_down_helper(leds, NUM_INNER);
+    
+}  // end of rotate_down_helper
+
+
+/*===============================================================================
+ * Function:  rotate_inner_counter-clockwise
+ */
+void rotate_inner_counter-clockwise( void )
+{
+    rotate_up_helper(leds, NUM_INNER);
+    
+}  // end of rotate_down_helper
+
+/*===============================================================================
+ * Function:  rotate_outer_clockwise
+ */
+void rotate_outer_clockwise( void )
+{
+    rotate_down_helper(&(leds[OUTER_START]), NUM_OUTER);
+    
+}  // end of rotate_down_helper
+    
+
+/*===============================================================================
+ * Function:  rotate_outer_counter-clockwise
+ */
+void rotate_outer_counter-clockwise( void )
+{
+    rotate_up_helper(&(leds[OUTER_START]), NUM_OUTER);
+    
+}  // end of rotate_down_helper
 
 void setup()
 {
@@ -27,6 +182,7 @@ void setup()
     FastLED.delay(1000);
 
     leds[0] = CRGB::Red;
+    leds[16]= CRGB::Red;
     
     FastLED.delay(1000);
 
@@ -35,20 +191,9 @@ void setup()
 #define LOOP_TIME 1000
 void loop()
 {
-    static int current_led=0;
-    int next_led;
-    CRGB temp;
 
-    // This loop is going to walk the an LED around the circle.  
-    next_led = current_led + 1;
-    if (next_led == NUM_LEDS) next_led = 0;
-
-    temp = leds[next_led];
-    leds[next_led] = leds[current_led];
-    leds[current_led] = temp;
-
-    current_led++;
-    if (current_led == NUM_LEDS) current_led = 0;
+    rotate_inner_clockwise();
+    rotate_outer_clockwise();
     
     FastLED.show();
     FastLED.delay(LOOP_TIME);
